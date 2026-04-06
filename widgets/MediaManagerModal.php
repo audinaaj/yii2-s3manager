78e6611fc05d905a345911b33fca3ec687d1c1cf
AJ Davis <ajdavis@audina.net>
2026-04-06 12:19:46 -0400

    Add credentials and endpoint to the MediaManagerModal widget.

--- widgets/MediaManagerModal.php
+++ widgets/MediaManagerModal.php
@@ -28,6 +28,16 @@ class MediaManagerModal extends Widget
     public $s3prefix = null;
 
     /**
+     * @var array $s3credentials AWS credentials with 'key' and 'secret'
+     */
+    public $s3credentials = null;
+
+    /**
+     * @var string $s3endpoint Custom S3 endpoint for non-AWS providers (e.g., DigitalOcean Spaces)
+     */
+    public $s3endpoint = null;
+
+    /**
      * @inheritdoc
      */
     public function init()
@@ -37,25 +47,35 @@ class MediaManagerModal extends Widget
         if ($this->s3bucket === null) {
             $this->s3bucket = isset(\Yii::$app->params['s3bucket'])
                 ? \Yii::$app->params['s3bucket']
-                : \Yii::$app->modules['s3manager']['configuration']['bucket'];
+                : \Yii::$app->modules['s3manager']->configuration['bucket'];
         }
 
         if ($this->s3region === null) {
             $this->s3region = isset(\Yii::$app->params['s3region'])
                 ? \Yii::$app->params['s3region']
-                : \Yii::$app->modules['s3manager']['configuration']['region'];
+                : \Yii::$app->modules['s3manager']->configuration['region'];
         }
 
         if ($this->s3prefix === null) {
             if (isset(\Yii::$app->params['s3prefix'])) {
                 $this->s3prefix = \Yii::$app->params['s3prefix'];
+            } elseif (isset(\Yii::$app->modules['s3manager']->configuration['prefix'])) {
+                $this->s3prefix = \Yii::$app->modules['s3manager']->configuration['prefix'];
             }
+        }
 
-            if (isset(\Yii::$app->modules['s3manager']['configuration']['s3prefix'])) {
-                $this->s3prefix = \Yii::$app->modules['s3manager']['configuration']['s3prefix'];
+        if ($this->s3credentials === null) {
+            if (isset(\Yii::$app->params['s3credentials'])) {
+                $this->s3credentials = \Yii::$app->params['s3credentials'];
+            } elseif (isset(\Yii::$app->modules['s3manager']->configuration['credentials'])) {
+                $this->s3credentials = \Yii::$app->modules['s3manager']->configuration['credentials'];
             }
+        }
 
-            $this->s3prefix = null;
+        if ($this->s3endpoint === null) {
+            if (isset(\Yii::$app->modules['s3manager']->configuration['endpoint'])) {
+                $this->s3endpoint = \Yii::$app->modules['s3manager']->configuration['endpoint'];
+            }
         }
     }
 
@@ -70,6 +90,16 @@ class MediaManagerModal extends Widget
         \Yii::$app->session->set(skyS3Module::SESSION_BUCKET_KEY, $this->s3bucket);
         \Yii::$app->session->set(skyS3Module::SESSION_REGION_KEY, $this->s3region);
         \Yii::$app->session->set(skyS3Module::SESSION_PREFIX_KEY, $this->s3prefix);
+        
+        // Set credentials in session if available
+        if ($this->s3credentials !== null) {
+            \Yii::$app->session->set(skyS3Module::SESSION_BUCKET_KEY . '_creds', $this->s3credentials);
+        }
+        
+        // Set endpoint in session if available
+        if ($this->s3endpoint !== null) {
+            \Yii::$app->session->set(skyS3Module::SESSION_BUCKET_KEY . '_endpoint', $this->s3endpoint);
+        }
 
         return $this->renderFile('@vendor/skylineos/yii2-s3manager/views/default/modal.php', []);
     }
